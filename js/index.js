22a3297564e25b509f6500737c160255e9a54a9f
rdwebdev24 <rohitdhakad101@gmail.com>
2022-11-24 20:20:10 +0530

    completed

--- js/index.js
+++ js/index.js
@@ -1,5 +1,53 @@
 let skillcards = document.querySelector('.skillcards');
-let projectcard = document.querySelector('.projectcard');
+let projectcard = document.querySelector('.projectcards');
+let landingwrapper = document.querySelector('.section1');
+let contactWrapper = document.querySelector('.section4');
+let skillsWrapper = document.querySelector('.section2');
+let projectWrapper = document.querySelector('.section3');
+let cssbtn1 = document.querySelector('.landngcssbtn');
+let cssbtn2 = document.querySelector('.skillcssbtn');
+let cssbtn3 = document.querySelector('.projectcssbtn');
+let label = document.querySelectorAll('.form-check-label');
+let landingcss = false;
+let skillcss = false;
+let projectcss = false;
+
+cssbtn1.addEventListener('click',(e)=>{
+     if(!landingcss){
+          landingwrapper.classList.add("landingwrapper")
+          label[0].textContent = "Remove css"
+          landingcss=true;
+     }else{
+          landingwrapper.classList.remove("landingwrapper")
+          label[0].textContent = "Apply css"
+          landingcss=false;
+     }
+})
+cssbtn2.addEventListener('click',(e)=>{
+     if(!skillcss){
+          skillsWrapper.classList.add("skillsWrapper")
+          label[1].textContent = "Remove css"
+          skillcss=true;
+     }else{
+          skillsWrapper.classList.remove("skillsWrapper")
+          label[1].textContent = "Apply css"
+          skillcss=false;
+     }
+})
+cssbtn3.addEventListener('click',(e)=>{
+     if(!projectcss){
+          projectWrapper.classList.add("projectWrapper")
+          contactWrapper.classList.add("contactWrapper")
+          label[2].textContent = "Remove css"
+          projectcss=true;
+     }else{
+          projectWrapper.classList.remove("projectWrapper")
+          contactWrapper.classList.remove("contactWrapper")
+          label[2].textContent = "Apply css"
+          projectcss=false;
+     }
+})
+
 let skillArray = [
      "HTML",
      "CSS",
@@ -57,3 +105,4 @@ ProjectArray.map((item)=>{
                                    <a href="${link}">view</a>
                                   </div>`
 })
+
